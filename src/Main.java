0c96ec021c2116d1589ca665d6397ec596b20f1d
rgnap <osovskaya_1993@mail.ru>
2022-11-14 18:07:33 +0300

    Commit1. Finished task 1.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import javax.sound.sampled.SourceDataLine;
+
 public class Main {
     public static void main(String[] args) {
 
+     //task1
+        int age = 18;
+        if (age >= 18);{
+            System.out.println("Поздравляем с совершеннолетием!");
+        }
+        if (age < 18);{
+            System.out.println("Вам еще не исполнилось 18,осталось подождать еще немного :)");
+        }
+
+
+
+
+
+
+
+
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
